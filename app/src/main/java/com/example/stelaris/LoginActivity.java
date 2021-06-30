8e8ec0acada9971bcaa4ff254992d48b87496edb
BorjaPalacios <33701366+BorjaPalacios@users.noreply.github.com>
2021-06-30 09:35:10 +0200

    activity sign up xml

--- app/src/main/java/com/example/stelaris/LoginActivity.java
+++ app/src/main/java/com/example/stelaris/LoginActivity.java
@@ -3,12 +3,23 @@ package com.example.stelaris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username,  password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.username = findViewById(R.id.txtuserName);
+        this.password = findViewById(R.id.txtPassword);
+    }
+
+    public void sigin(View view){
+
     }
 }
