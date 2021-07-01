bee27f47574a3717727102a0c1a101bf7c4267ba
BorjaPalacios <33701366+BorjaPalacios@users.noreply.github.com>
2021-07-01 13:22:46 +0200

    seleccionar foto

--- app/src/main/java/com/example/stelaris/activities/SignUpActivity.java
+++ app/src/main/java/com/example/stelaris/activities/SignUpActivity.java
@@ -1,9 +1,14 @@
 package com.example.stelaris.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.stelaris.R;
@@ -15,6 +20,8 @@ import com.google.android.material.snackbar.Snackbar;
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText username, password, email;
+    private ImageView image;
+    private final int PICK_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,23 @@ public class SignUpActivity extends AppCompatActivity {
 
         this.password = findViewById(R.id.txtPasswordSign);
         this.password.setOnFocusChangeListener(listenerPassword());
+
+        this.image = findViewById(R.id.imageView2);
+    }
+
+    public void getImage(View view){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(i, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            Uri imageUri = data.getData();
+            this.image.setImageURI(imageUri);
+        }
     }
 
     public void registrar(View view) {
