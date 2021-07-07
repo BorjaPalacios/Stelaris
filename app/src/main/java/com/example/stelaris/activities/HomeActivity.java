af2992caee810ca2a4fc394bbf04f04725ea96a1
BorjaPalacios <33701366+BorjaPalacios@users.noreply.github.com>
2021-07-07 10:45:48 +0200

    menu

--- app/src/main/java/com/example/stelaris/activities/HomeActivity.java
+++ app/src/main/java/com/example/stelaris/activities/HomeActivity.java
@@ -1,9 +1,17 @@
 package com.example.stelaris.activities;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.stelaris.R;
@@ -12,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView celeste, descrpicion;
     private ImageView imagenNasa;
+    private Button btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,41 @@ public class HomeActivity extends AppCompatActivity {
         this.celeste = findViewById(R.id.lblCeleste);
         this.descrpicion = findViewById(R.id.lblDescripcion);
         this.imagenNasa = findViewById(R.id.imgNasa);
+        this.btnMenu = findViewById(R.id.btnMenu);
+
+        registerForContextMenu(btnMenu);
+        this.btnMenu.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                v.showContextMenu(1000, 0);
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        Toast toast = null;
+
+        switch (item.getItemId()) {
+            case R.id.menuPerfil:
+                toast = Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT);
+                break;
+            case R.id.menuOpciones:
+                toast = Toast.makeText(this, "Ajustes", Toast.LENGTH_SHORT);
+                break;
+            case R.id.menuCerrarSesion:
+                toast = Toast.makeText(this, "Cerrar Sesion", Toast.LENGTH_SHORT);
+                break;
+        }
+
+        toast.show();
+
+        return true;
     }
 }
